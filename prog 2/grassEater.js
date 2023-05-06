19f47fa2efce2b551ac5efa009f8a60d3c2ec692
TatevMargaryan0331 <tatev.margaryan1.y@tumo.org>
2023-05-06 14:25:54 +0400

    prog 2 fin

---
+++ prog 2/grassEater.js
@@ -0,0 +1,113 @@
+class GrassEater {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.energy = 8;
+        this.index = index;
+        this.directions = [[this.x - 1, this.y - 1],
+        [this.x, this.y - 1],
+        [this.x + 1, this.y - 1],
+        [this.x - 1, this.y],
+        [this.x + 1, this.y],
+        [this.x - 1, this.y + 1],
+        [this.x, this.y + 1],
+        [this.x + 1, this.y + 1]];
+
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+
+            }
+        }
+        return found;
+
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    die() {
+        console.log("merav");
+
+        matrix[this.y][this.x] = 0
+        for (var i in grassEaterArr) {
+            if (this.x == grassEaterArr[i].x && this.y == grassEaterArr[i].y) {
+                grassEaterArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    eat() {
+
+        let foods = this.chooseCell(1)
+        let food = random(foods)
+        if (food) {
+            this.energy++
+            matrix[this.y][this.x] = 0
+            let newX = food[0]
+            let newY = food[1]
+            matrix[food[1]][food[0]] = 2
+            this.x = newX
+            this.y = newY
+            for (var i in grassArr) {
+                if (newX == grassArr[i].x && newY == grassArr[i].y) {
+                    grassArr.splice(i, 1);
+                    break;
+                }
+            }
+            if (this.energy >= 12) {
+                this.mul()
+            }
+        }
+        else {
+            this.move()
+        }
+    }
+
+    move() {
+
+        this.energy--;
+        var newCell = random(this.chooseCell(0));
+
+        if (newCell) {
+            matrix[this.y][this.x] = 0;
+            matrix[newCell[1]][newCell[0]] = 2;
+            this.x = newCell[0];
+            this.y = newCell[1];
+        }
+        if (this.energy <= 0) {
+            this.die()
+        }
+    }
+
+    mul() {
+        var newCell = random(this.chooseCell(0));
+        if (newCell) {
+            var newGrassEater = new GrassEater(newCell[0], newCell[1], this.index);
+            grassEaterArr.push(newGrassEater);
+            matrix[newCell[1]][newCell[0]] = 2;
+            this.energy = 8
+        }
+    }
+}
+
---
+++ prog 2/script.js
@@ -0,0 +1,143 @@
+var matrix = []
+var n = +prompt("nshel matrix-i laynutyuny")
+var m = +prompt("nshel matrix-i erkarutyuny")
+
+if (n < 50) {
+    n = 0
+    alert("matrix-i laynutyuny chpetq e lini 50-ic poqr! nsheq nory");
+    n = +prompt("nshel matrix-i laynutyuny")
+}
+if (m < 50) {
+    m = 0
+    alert("matrix-i erkarutyuny chpetq e lini 50-ic poqr! nsheq nory");
+    m = +prompt("nshel matrix-i erkarutyuny")
+}
+
+// var side = 120;
+var side = 10;
+var grassArr = [];
+var grassEaterArr = [];
+var OmnivoreArr = [];
+var OmnivoreEaterArr = [];
+var HunterArr = [];
+
+function kerparner(qanak, kerpar) {
+    var a = 0;
+    while (a<qanak) {
+        var x = Math.floor(random(0,m))
+        var y = Math.floor(random(0,n))
+        
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = kerpar;
+        }
+        else {
+            var x = Math.floor(random(0,m))
+            var y = Math.floor(random(0,n))
+         
+            matrix[y][x] = kerpar;
+        }
+        a++;
+    }
+}
+
+
+function setup() {
+
+
+    for (let i = 0; i < n; i++) {
+        matrix[i] = []
+        for (let j = 0; j < m; j++) {
+            matrix[i].push(0)
+        }
+    }
+
+    kerparner(100, 1);
+    kerparner(300, 2);
+    kerparner(100, 3);
+    kerparner(100, 4);
+    kerparner(100, 5);
+    
+    frameRate(5);
+    createCanvas(matrix[0].length * side, matrix.length * side);
+    background('#acacac');
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                let grass = new Grass(x, y, 1);
+                grassArr.push(grass)
+            }
+            else if (matrix[y][x] == 2) {
+                let grassEater = new GrassEater(x, y, 2);
+                grassEaterArr.push(grassEater)
+            }
+            else if (matrix[y][x] == 3) {
+                let omnivore = new Omnivore(x, y, 3);
+                OmnivoreArr.push(omnivore)
+            }
+            else if (matrix[y][x] == 4) {
+                let newOmnivoreEater = new OmnivoreEater(x, y, 4);
+                OmnivoreEaterArr.push(newOmnivoreEater)
+            }
+            else if (matrix[y][x] == 5) {
+                let hunter = new Hunter(x, y, 4);
+                HunterArr.push(hunter)
+            }
+        }
+    }
+    // console.log(grassArr);
+}
+
+
+function draw() {
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+            }
+            else if (matrix[y][x] == 3) {
+                fill("red");
+            }
+            else if (matrix[y][x] == 4) {
+                fill("brown");
+            }
+            else if (matrix[y][x] == 5) {
+                fill("black");
+            }
+
+            rect(x * side, y * side, side, side);
+
+            // fill("blue")
+            // text(x + " " + y, x * side + side / 2, y * side + side / 2)
+        }
+    }
+
+    for (var i in grassArr) {
+        grassArr[i].mul();
+    }
+
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].eat();
+    }
+
+    for (var i in OmnivoreArr) {
+        OmnivoreArr[i].eat();
+    }
+
+    for (var i in OmnivoreEaterArr) {
+        OmnivoreEaterArr[i].eat();
+    }
+
+    for (var i in HunterArr) {
+        HunterArr[i].kill();
+    }
+
+}
