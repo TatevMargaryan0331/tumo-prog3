2019dac8f1ab453a819318723f3d86c1051b3739
TatevMargaryan0331 <tatev.margaryan1.y@tumo.org>
2023-05-22 20:32:22 +0400

    upgrade

--- prog2/client/script.js
+++ prog2/client/script.js
@@ -1,5 +1,37 @@
+var socket = io();
+socket.on("my_matrix", my_Draw)
+
 function setup() {
     frameRate(5);
-    createCanvas(matrix[0].length * side, matrix.length * side);
+    createCanvas(500, 500);
     background('#acacac');
+}
+
+function my_Draw(matrix) {
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill("green");
+            }
+            else if (matrix[y][x] == 0) {
+                fill("#acacac");
+            }
+            else if (matrix[y][x] == 2) {
+                fill("yellow");
+            }
+            else if (matrix[y][x] == 3) {
+                fill("red");
+            }
+            else if (matrix[y][x] == 4) {
+                fill("brown");
+            }
+            else if (matrix[y][x] == 5) {
+                fill("black");
+            }
+
+            rect(x * 10, y * 10, 10, 10);
+       }
+    }
 }
--- prog2/server/server.js
+++ prog2/server/server.js
@@ -1,43 +1,44 @@
 var express = require("express");
-
 var app = express();
 
 app.use(express.static("../client"));
-
 app.get("/", function (req, res) {
-
     res.redirect("index.html");
-
 });
 
-app.listen(3000, function () {
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
+server.listen(3000, function () {
     console.log("Game is running on port 3000");
-
 });
 
-var Grass = require("./my_modules/grass");
-var GrassEater = require("./my_modules/grassEater");
-var Hunter = require("./my_modules/hunter");
-var Omnivore = require("./my_modules/omnivore");
-var OmnivoreEater = require("./my_modules/omnivoreEater");
-
-var matrix = []
-var n = 100;
-var m = 100;
+matrix = [];
+n = 50;
+m = 50;
+Grass = require("./my_modules/grass");
+GrassEater = require("./my_modules/grassEater");
+Hunter = require("./my_modules/hunter");
+Omnivore = require("./my_modules/omnivore");
+OmnivoreEater = require("./my_modules/omnivoreEater");
+grassArr = [];
+grassEaterArr = [];
+OmnivoreArr = [];
+OmnivoreEaterArr = [];
+HunterArr = [];
 
 function kerparner(qanak, kerpar) {
     var a = 0;
     while (a < qanak) {
-        var x = Math.floor(random(0, m))
-        var y = Math.floor(random(0, n)) // 
+        var x = Math.floor(Math.random() * m)
+        var y = Math.floor(Math.random() * n) // 
 
         if (matrix[y][x] == 0) {
             matrix[y][x] = kerpar;
         }
         else {
-            var x = Math.floor(random(0, m))
-            var y = Math.floor(random(0, n))
+            var x = Math.floor(Math.random() * m)
+            var y = Math.floor(Math.random() * n)
 
             matrix[y][x] = kerpar;
         }
@@ -45,7 +46,7 @@ function kerparner(qanak, kerpar) {
     }
 }
 
-function generateMatrix(){
+function generateMatrix() {
 
     for (let i = 0; i < n; i++) {
         matrix[i] = []
@@ -62,4 +63,64 @@ function generateMatrix(){
 
 }
 
+function creatObjects() {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                let grass = new Grass(x, y, 1);
+                grassArr.push(grass)
+            }
+            else if (matrix[y][x] == 2) {
+                let grassEater = new GrassEater(x, y, 2);
+                grassEaterArr.push(grassEater)
+            }
+            else if (matrix[y][x] == 3) {
+                let omnivore = new Omnivore(x, y, 3);
+                OmnivoreArr.push(omnivore)
+            }
+            else if (matrix[y][x] == 4) {
+                let newOmnivoreEater = new OmnivoreEater(x, y, 4);
+                OmnivoreEaterArr.push(newOmnivoreEater)
+            }
+            else if (matrix[y][x] == 5) {
+                let hunter = new Hunter(x, y, 4);
+                HunterArr.push(hunter)
+            }
+        }
+    }
+
+}
+
+function StartGame(){
+
+    // for (var i in grassArr) {
+    //     grassArr[i].mul();
+    // }
+
+    // for (var i in grassEaterArr) {
+    //     grassEaterArr[i].eat();
+    // }
+
+    // for (var i in OmnivoreArr) {
+    //     OmnivoreArr[i].eat();
+    // }
+
+    // for (var i in OmnivoreEaterArr) {
+    //     OmnivoreEaterArr[i].eat();
+    // }
+
+    // for (var i in HunterArr) {
+    //     HunterArr[i].kill();
+    // } 
+
+    io.sockets.emit("my_matrix", matrix);
+
+}
+
 generateMatrix();
+creatObjects();
+setInterval(StartGame, 1000);
+
+io.on('connection', function (socket) {
+    socket.emit("my_matrix", matrix);
+});
